99b52c586d72c55dc1cd51db2c0d6d4a7391b8fb
tontorot <shiga_saga_nara@yahoo.co.jp>
2017-02-11 22:45:44 +0900

    find out 3 cats, then display complete image

--- canvas.js
+++ canvas.js
@@ -4,7 +4,20 @@ $(function () {
   var context = canvas.getContext('2d');
   var background_img = new Image();
   background_img.src = "image.jpg";
-  context.drawImage(background_img,0,0);
+  background_img.onload = function() {
+    context.drawImage(background_img,0,0);
+  }
+  //当たり判定用配列
+  //連想配列
+  var data ={
+      0:{"x":100,"y":150,"is_open":false}
+      ,1:{"x":0,"y":0,"is_open":false}
+      ,2:{"x":320,"y":240,"is_open":false}
+  };
+
+  var width = 190;
+  var height = 190;
+
   function onDown(e) {
     console.log("down");
   }
@@ -16,7 +29,42 @@ $(function () {
     var x = e.clientX - canvas.offsetLeft;
     var y = e.clientY - canvas.offsetTop;
     console.log("x:", x, "y:", y);
-    drawCat(x, y);
+    openCheck(x,y);
+  }
+  function openCheck(x,y)
+  {
+    var is_complete = true;
+    for(var key in data)
+    {
+      if(!data[key]["is_open"])
+      {
+        var hit_point_x = data[key]["x"];
+        var hit_point_y = data[key]["y"];
+        if( hit_point_x - width/2 < x && x < hit_point_x + width/2 &&
+            hit_point_y - height/2 < y && y < hit_point_y + height/2)
+        {
+          console.log("is_clicked, drawCat at ("+hit_point_x+","+hit_point_y+")");
+          drawCat(hit_point_x,hit_point_y);
+          data[key]["is_open"] = true;
+        }
+        else
+        {
+          is_complete = false;
+        }
+      }
+    }
+    //フラグが折れていなかったら、complete画像を表示
+    if(is_complete)
+    {
+      var complete_img = new Image();
+      complete_img.src = "complete.jpg";
+      /* 画像を描画 */
+      complete_img.onload = function() {
+        console.log("img.width = "+complete_img.width);
+        console.log("img.height = "+complete_img.height);
+        context.drawImage(complete_img, 320 - complete_img.width/2, 240 - complete_img.height/2);
+      }
+    }
   }
   function onOver(e) {
     console.log("mouseover");
@@ -25,13 +73,14 @@ $(function () {
     console.log("mouseout");
   }
   function drawCat(x, y) {
-//    context.fillRect(x, y, width, height);
     var img = new Image();
     img.src = "cat_test.png";
-    var width = 190;
-    var height = 190;
+    //gifは動かなかった
+    //img.src = "mew_cat.gif";
     /* 画像を描画 */
-    context.drawImage(img, x - width/2, y - height/2);
+    img.onload = function() {
+      context.drawImage(img, x - width/2, y - height/2);
+    }
   }
   canvas.addEventListener('mousedown', onDown, false);
   canvas.addEventListener('mouseup', onUp, false);
