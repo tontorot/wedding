a6c06f56626f764a2abc61323cc769917df8c27e
tontorot <shiga_saga_nara@yahoo.co.jp>
2017-07-17 14:56:18 +0900

    捜査官をだいぶ修正

--- game/game.js
+++ game/game.js
@@ -230,20 +230,36 @@ console.log("background_image_width = "+background_image_width);
       hidden_cat_data[index]["image_buff"].alpha = 0.5;
       hidden_cat_data[index]["image_buff"].addEventListener('mousedown',catsClicked(index),false);
       hidden_cat_data[index]["image_buff"].addEventListener('touchstart',catsClicked(index),false);
+      hidden_cat_data[index]["image_buff"].addEventListener('pressup',catsPressup(index),false);
+      // hidden_cat_data[index]["image_buff"].addEventListener('touchend',catsClicked(index),false);
     }
   }
-
+  var is_cat_pressed = false;
   function catsClicked(index)
   {
     return function(event)
     {
+      console.log("EVENT:catsClicked at "+index);
+      is_cat_pressed = true;
+    }
+  }
+
+  function catsPressup(index)
+  {
+    return function(event)
+    {
+      console.log("EVENT:catsPressup at "+index);
+      is_cat_pressed = false;
       if(is_touch_forbidden)
+      {
+        return;
+      }
+      if(total_diff_x != before_total_diff_x)
       {
         return;
       }
       // 猫の説明を表示している間、他の猫をタッチできなくする
       is_touch_forbidden = true;
-      console.log("catsClicked at "+index);
       hidden_cat_data[index]["image_buff"].alpha = 1;
       // この猫は見つけられたというフラグを立てる
       hidden_cat_data[index]["is_open"] = true;
@@ -355,24 +371,27 @@ console.log("background_image_width = "+background_image_width);
   var before_x_for_move_container;
   var total_diff_x = 0;
   var total_diff_y = 0;
+  var before_total_diff_x;
   function onDown(e) {
+    console.log("EVENT:onDown");
     in_drag = true;
     before_x_for_move_container = e.clientX - canvas.offsetLeft;
     before_x_for_click = before_x_for_move_container;
-
-    showTouchEffect(container, e.clientX-canvas_left_offset-total_diff_x, e.clientY-canvas_top_offset-total_diff_y);
-    checkCatDistance(e.clientX-canvas_left_offset-total_diff_x, e.clientY-canvas_top_offset-total_diff_y);
-  }
+    before_total_diff_x = total_diff_x;
+ }
   function touchStart(e) {
+    console.log("EVENT:touchStart");
     in_drag = true;
     before_x_for_move_container = e.touches[0].clientX - canvas.offsetLeft;
     before_x_for_click = before_x_for_move_container;
-
-    showTouchEffect(container, e.touches[0].clientX-canvas_left_offset-total_diff_x, e.touches[0].clientY-canvas_top_offset-total_diff_y);
-    checkCatDistance(e.touches[0].clientX-canvas_left_offset-total_diff_x, e.touches[0].clientY-canvas_top_offset-total_diff_y);
+    before_total_diff_x = total_diff_x;
   }
   function checkCatDistance(x,y)
   {
+    if(total_diff_x != before_total_diff_x)
+    {
+      return;
+    }
     //現実世界系からcanvas座標系へ変換
     x /= resize_ratio;
     y /= resize_ratio;
@@ -389,7 +408,7 @@ console.log("background_image_width = "+background_image_width);
         // console.log("Math.pow(cat_x-x,2) = "+Math.pow(cat_x-x,2));
         // console.log("Math.pow(cat_y-y,2) = "+Math.pow(cat_y-y,2));
         var distance = Math.sqrt(Math.pow(cat_x-x,2)+Math.pow(cat_y-y,2));
-        console.log("index = "+index+", distance = "+distance);
+        // console.log("index = "+index+", distance = "+distance);
         if(shortest_distance > distance)
         {
           shortest_distance = distance;
@@ -402,7 +421,10 @@ console.log("background_image_width = "+background_image_width);
     {
       return;
     }
-
+    if(is_cat_pressed)
+    {
+      return;
+    }
     //　猫レーダーの感度によって、表示する反応をかえる
     if(shortest_distance < 100)
     {
@@ -414,32 +436,24 @@ console.log("background_image_width = "+background_image_width);
     }
   }
   function onUp(e) {
+    console.log("EVENT:onUP");
     in_drag = false;
-    // var touched_x = e.clientX - total_diff_x;
-    // var touched_y = e.clientY - total_diff_y;
-    var touched_x = e.clientX;
-    var touched_y = e.clientY;
-    console.log("total_diff_x = "+total_diff_x);
-    var calc_right = e.clientX - canvas.offsetLeft; 
-    if(before_x_for_click == calc_right)
+    if(total_diff_x != before_total_diff_x)
     {
-      // openCheck(touched_x, touched_y);
-      change_floor(touched_x, touched_y);
+      return;
     }
+    showTouchEffect(container, e.clientX-canvas_left_offset-total_diff_x, e.clientY-canvas_top_offset-total_diff_y);
+    checkCatDistance(e.clientX-canvas_left_offset-total_diff_x, e.clientY-canvas_top_offset-total_diff_y);
   }
   function touchEnd(e) {
+    console.log("EVENT:touchEnd");
     in_drag = false;
-    // var touched_x = e.touches[0].clientX - total_diff_x;
-    // var touched_y = e.touches[0].clientY - total_diff_y;
-    var touched_x = e.touches[0].clientX;
-    var touched_y = e.touches[0].clientY;
-    console.log("total_diff_x = "+total_diff_x);
-    var calc_right = e.touches[0].clientX - canvas.offsetLeft;
-    if(before_x_for_click == calc_right)
+    if(total_diff_x != before_total_diff_x)
     {
-      // openCheck(touched_x, touched_y);
-      change_floor(touched_x, touched_y);
+      return;
     }
+    showTouchEffect(container, e.touches[0].clientX-canvas_left_offset-total_diff_x, e.touches[0].clientY-canvas_top_offset-total_diff_y);
+    checkCatDistance(e.touches[0].clientX-canvas_left_offset-total_diff_x, e.touches[0].clientY-canvas_top_offset-total_diff_y);
   }
   function onMove(e) {
     moveContainer_x(e.clientX)
@@ -491,130 +505,6 @@ console.log("background_image_width = "+background_image_width);
     var y = e.clientY - canvas.offsetTop;
   }
 
-  var is_change_floor_forbidden = false;
-  function change_floor(x,y)
-  {
-    //　二重発火対策
-    if(is_change_floor_forbidden)
-    {
-      return;
-    }
-    is_change_floor_forbidden = true;
-    switch(now_floor)
-    {
-      case 1:
-        // 2Fへ向かう当たり判定を踏んでいたら、2Fへ移動
-        if(to_upstairs_rectangle["x"] * resize_ratio < x &&
-           x < (to_upstairs_rectangle["x"] + to_upstairs_rectangle["width"]) * resize_ratio &&
-           to_upstairs_rectangle["y"] * resize_ratio < y &&
-           y < (to_upstairs_rectangle["y"] + to_upstairs_rectangle["height"]) * resize_ratio)
-        {
-          createjs.Tween.get(move_container).to({y:background_image_height}, 1000);
-          total_diff_y = background_image_height;
-          now_floor = 2;
-          stage.update();
-        }
-        break;
-      case 2:
-        // 1Fへ向かう当たり判定を踏んでいたら、1Fへ移動
-        if(to_downstairs_rectangle["x"] * resize_ratio < x &&
-           x < (to_downstairs_rectangle["x"] + to_downstairs_rectangle["width"]) * resize_ratio &&
-           to_downstairs_rectangle["y"] * resize_ratio < y &&
-           y < (to_downstairs_rectangle["y"] + to_downstairs_rectangle["height"]) * resize_ratio)
-        {
-          createjs.Tween.get(move_container).to({y:0}, 1000);
-          total_diff_y = 0;
-          now_floor = 1;
-          stage.update();
-        }
-        break;
-    }
-    var hoge = setInterval(function() {
-      is_change_floor_forbidden = false;
-      clearInterval(hoge);
-    }, 1000);
-    console.log("total_diff_y = "+total_diff_y);
-
-  }
-  function openCheck(x,y)
-  {
-    console.log("openCheck(x,y) = ("+x+","+y+")");
-    if(is_touch_forbidden)
-    {
-      return;
-    }
-    //猫の説明画面が出ていれば、それを削除する
-    var ret = container3.removeChild(cat_discription_image);
-    //猫の説明画像を削除するのに成功した場合
-    if(ret)
-    {
-      is_complete();
-      // ダンボール画像を削除して、開封後画像に置換
-      // container3.removeChild(hidden_cat_data[keeped_cat_index]["image_buff"]);
-      // added_image = addImage(container3, hidden_cat_data[keeped_cat_index]["open_image"], hidden_cat_data[keeped_cat_index]["hit_offset_x"], hit_offset_y, cat_list_resize_ratio);
-      // 猫の説明画像が出ていて、削除された場合は、以降の猫探索処理を実行しない
-      return;
-    }
-    keeped_cat_index = null;
-    var is_once_open = false;
-    for(var index in hidden_cat_data)
-    {
-      var hidden_cat = hidden_cat_data[index];
-      if(is_once_open)
-      {
-        continue;
-      }
-      if(hidden_cat["is_open"])
-      {
-        continue;
-      }
-      var cat_image_width = hidden_cat["image_width"]; //クリックされた位置にでるネコ画像の横幅
-      var cat_image_height = hidden_cat["image_height"]; //クリックされた位置にでるネコ画像の縦幅
-      var hit_point_x = hidden_cat["x"]; //画像左上のx座標
-      var hit_point_y = hidden_cat["y"]; //画像左上のy座標
-
-      var calc_width=hit_point_x+cat_image_width;
-      var calc_height=hit_point_y+cat_image_height;
-console.log(hidden_cat);
-console.log("hit_point_x = "+hit_point_x+", x = "+x+", width = "+calc_width);
-console.log("hit_point_t = "+hit_point_y+", y = "+y+", height = "+calc_height);
-      // 画像もresize_ratioの分引き伸ばされているので、それを加味した当たり判定チェック
-      if( hit_point_x < x && x < calc_width &&
-          hit_point_y < y && y < calc_height)
-      {
-        // 探し当てられた猫を表示する
-        var get_cat = addImage(container,hidden_cat["open_image"],hidden_cat["x"],hidden_cat["y"]);
-        // この猫は見つけられたというフラグを立てる
-        hidden_cat["is_open"] = true;
-        // 今回のタップにおいては、二匹目以降の発見チェックをしないというフラグを立てる
-        is_once_open = true;
-
-        is_touch_forbidden = true;
-        // 1秒後に説明を表示する猫番号を一時変数に格納
-        keeped_cat_index = index;
-        // 1秒後に猫説明を表示
-        var hoge = setInterval(function() {
-          // 見つかった猫は消して
-          // container.removeChild(get_cat);
-          // 猫の説明を表示する
-          cat_discription_image = addImage(container3, hidden_cat["discription_image"], 0, 0);
-          is_touch_forbidden = false;
-          clearInterval(hoge);
-        }, 1000);
-
-        container3.removeChild(progress_image);
-        var found_cat_count = 0;
-        for(var index2 in hidden_cat_data)
-        {
-          if(hidden_cat_data[index2]["is_open"])
-          {
-            found_cat_count ++;
-          }
-        }
-        progress_image　= addImage(container3, found_cat_count+"_4", 0, 0, 0.2);
-      }
-    }
-  }
   //猫画像がすべてオープンになっているかチェックする
   function is_complete()
   {
